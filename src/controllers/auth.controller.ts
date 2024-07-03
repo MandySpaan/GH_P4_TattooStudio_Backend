a43e02cb04bd7a6341711e647de094397498ffc6
MandySpaan <mandyspaan94@gmail.com>
2024-07-03 21:49:01 +0200

    feat: Scaffold endpoints authentication

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from "express";
+
+export const register = (req: Request, res: Response) => {
+  res.send("Register code to be written");
+};
+
+export const login = (req: Request, res: Response) => {
+  res.send("Login code to be written");
+};
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
-import 'dotenv/config';
-import express, { Request, Response } from 'express';
+import "dotenv/config";
+import express, { Request, Response } from "express";
+import { login, register } from "./controllers/auth.controller";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
-app.get('/healthy', (req: Request, res: Response) => {
-    res.send('Server is healthy')
-})
+app.get("/healthy", (req: Request, res: Response) => {
+  res.send("Server is healthy");
+});
 
+// Authentication
+app.post("/api/auth/register", register);
+app.post("/api/auth/login", login);
+
+// Server running
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
