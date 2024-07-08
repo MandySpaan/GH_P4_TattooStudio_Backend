acce0178f27e04673eb2084c0c7575f20b8bede1
MandySpaan <mandyspaan94@gmail.com>
2024-07-08 21:51:32 +0200

    feat(seeders): roleSeeder

---
+++ src/database/seeders/dbSeeder.ts
@@ -0,0 +1,6 @@
+import { roleSeeder } from "./roleSeeder";
+
+(async () => {
+  console.log("Starting seeders...");
+  await roleSeeder();
+})();
---
+++ src/database/seeders/roleSeeder.ts
@@ -0,0 +1,27 @@
+import { AppDataSource } from "../db";
+import { Role } from "../models/Role";
+
+export const roleSeeder = async () => {
+  try {
+    await AppDataSource.initialize();
+
+    const superAdmin = new Role();
+    superAdmin.id = 1;
+    superAdmin.roleName = "super_admin";
+    await superAdmin.save();
+
+    const admin = new Role();
+    admin.id = 2;
+    admin.roleName = "admin";
+    await admin.save();
+
+    const user = new Role();
+    user.id = 3;
+    user.roleName = "user";
+    await user.save();
+  } catch (error: any) {
+    console.log("Error roleSeeder:", error.message);
+  } finally {
+    await AppDataSource.destroy();
+  }
+};
